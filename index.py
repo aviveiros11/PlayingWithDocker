fb404bd075e18e2878e0c1ad93b10481a90ad506
Austin Viveiros <viveiros.austin@gmail.com>
2020-02-24 15:35:55 -0700

    hmm

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
-def hello():
-    return "wow"
+def home():
+    return render_template('home.html')
 if __name__ == "__main__":
-    app.run(host="localhost", port=int("8000"), debug=True)
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
